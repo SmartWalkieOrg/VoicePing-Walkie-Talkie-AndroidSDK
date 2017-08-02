dce86d461ece79f776ebbee14b6d2080cbffa429
Kukuh <kukuhsain@gmail.com>
2017-08-02 13:23:30 +0700

    Simplify code for showing or hiding progress bar in LoginActivity

--- app/src/main/java/com/smartwalkie/voiceping/LoginActivity.java
+++ app/src/main/java/com/smartwalkie/voiceping/LoginActivity.java
@@ -1,11 +1,7 @@
 package com.smartwalkie.voiceping;
 
 import android.Manifest;
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -151,9 +147,8 @@ public class LoginActivity extends AppCompatActivity implements
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Intent activityIntent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(activityIntent);
                             showProgress(false);
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
                             finish();
                         }
                     });
@@ -167,39 +162,8 @@ public class LoginActivity extends AppCompatActivity implements
         }
     }
 
-    /**
-     * Shows the progress UI and hides the connect form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            connectFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            connectFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    connectFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            progressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            connectFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
+    private void showProgress(boolean show) {
+        progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        connectFormView.setVisibility(show ? View.GONE : View.VISIBLE);
     }
 }
