d407acbbf5c067c4a1702c944b5c6f6295db8033
Kukuh <kukuhsain@gmail.com>
2017-08-09 09:17:24 +0700

    Clean up Recorder

--- voiceping-sdk/src/main/java/com/smartwalkie/voicepingsdk/Recorder.java
+++ voiceping-sdk/src/main/java/com/smartwalkie/voicepingsdk/Recorder.java
@@ -39,8 +39,7 @@ public class Recorder implements OutgoingAudioListener {
 
     private static int state = STARTED;
 
-    private HandlerThread senderThread;
-    private Handler senderHandler;
+    private Handler mSenderHandler;
     private LinkedBlockingQueue<byte[]> mBlockingQueue;
 
     private static final int START = 1000;
@@ -65,12 +64,12 @@ public class Recorder implements OutgoingAudioListener {
 
     private void initSenderThread() {
         mBlockingQueue = new LinkedBlockingQueue<>();
-        senderThread = new HandlerThread("SenderThread", Thread.MAX_PRIORITY);
+        HandlerThread senderThread = new HandlerThread("SenderThread", Thread.MAX_PRIORITY);
         senderThread.start();
-        senderHandler = new Handler(senderThread.getLooper()) {
+        mSenderHandler = new Handler(senderThread.getLooper()) {
+
             @Override
             public void handleMessage(android.os.Message msg) {
-
                 switch (msg.what) {
                     case START:
                         sendAckStart();
@@ -114,16 +113,13 @@ public class Recorder implements OutgoingAudioListener {
         Log.v(TAG, "startTalking");
         this.receiverId = receiverId;
         this.channelType = channelType;
-//        sendAckStart();
-        senderHandler.sendEmptyMessage(START);
+        mSenderHandler.sendEmptyMessage(START);
     }
 
     public void stopTalking() {
         Log.v(TAG, "stopTalking");
-        senderHandler.removeMessages(CONTINUE_FOR_SENDING_AUDIO_DATA);
-//        stopRecording();
-//        sendAckStop();
-        senderHandler.sendEmptyMessage(STOP_NORMALLY);
+        mSenderHandler.removeMessages(CONTINUE_FOR_SENDING_AUDIO_DATA);
+        mSenderHandler.sendEmptyMessage(STOP_NORMALLY);
     }
 
     private void sendAckStart() {
@@ -137,14 +133,9 @@ public class Recorder implements OutgoingAudioListener {
 
     @Subscribe
     public void onAudioDataEvent(AudioDataEvent audioDataEvent) {
-        send(audioDataEvent.getData());
-    }
-
-    public void send(byte[] data) {
         try {
-            mBlockingQueue.put(data);
-//            sendAudio();
-            senderHandler.sendEmptyMessage(CONTINUE_FOR_SENDING_AUDIO_DATA);
+            mBlockingQueue.put(audioDataEvent.getData());
+            mSenderHandler.sendEmptyMessage(CONTINUE_FOR_SENDING_AUDIO_DATA);
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }
@@ -172,12 +163,11 @@ public class Recorder implements OutgoingAudioListener {
         byte[] message = MessageHelper.createAckStopMessage(userId, receiverId, channelType);
         mConnection.send(message);
         state = WAITING_FOR_ACK_END;
-        senderHandler.sendEmptyMessageDelayed(UPDATE_FOR_NOT_RECEIVED_ACK_END, ACK_TIMEOUT_IN_MILLIS);
+        mSenderHandler.sendEmptyMessageDelayed(UPDATE_FOR_NOT_RECEIVED_ACK_END, ACK_TIMEOUT_IN_MILLIS);
     }
 
     private void startRecording() {
         Log.v(TAG, "startRecording");
-//        isRecording = true;
         IS_RECORDING = true;
         mContext.startService(new Intent(mContext, RecorderService.class));
         state = RECORDING;
@@ -185,8 +175,6 @@ public class Recorder implements OutgoingAudioListener {
 
     private void stopRecording() {
         Log.v(TAG, "stopRecording");
-//        isRecording = false;
-//        mContext.stopService(new Intent(mContext, RecorderService.class));
         IS_RECORDING = false;
         mBlockingQueue.clear();
     }
@@ -199,33 +187,30 @@ public class Recorder implements OutgoingAudioListener {
     @Override
     public void onAckStartSucceed(Message message) {
         Log.v(TAG, "onAckStartSucceed: " + message);
-//        startRecording();
-        senderHandler.sendEmptyMessage(CONTINUE_FOR_RECEIVED_ACK_START);
+        mSenderHandler.sendEmptyMessage(CONTINUE_FOR_RECEIVED_ACK_START);
     }
 
     @Override
     public void onAckStartFailed(Message message) {
         Log.v(TAG, "onAckStartFailed: " + message);
-//        stopRecording();
-//        state = STOPPED;
-        senderHandler.sendEmptyMessage(STOP_FOR_RECEIVED_ACK_START_FAILED);
+        mSenderHandler.sendEmptyMessage(STOP_FOR_RECEIVED_ACK_START_FAILED);
     }
 
     @Override
     public void onAckEndSucceed(Message message) {
         Log.v(TAG, "onAckEndSucceed: " + message);
-        senderHandler.sendEmptyMessage(UPDATE_FOR_RECEIVED_ACK_END);
+        mSenderHandler.sendEmptyMessage(UPDATE_FOR_RECEIVED_ACK_END);
     }
 
     @Override
     public void onMessageDelivered(Message message) {
         Log.v(TAG, "onMessageDelivered: " + message);
-        senderHandler.sendEmptyMessage(UPDATE_FOR_RECEIVED_STATUS_DELIVERED);
+        mSenderHandler.sendEmptyMessage(UPDATE_FOR_RECEIVED_STATUS_DELIVERED);
     }
 
     @Override
     public void onMessageRead(Message message) {
         Log.v(TAG, "onMessageRead: " + message);
-        senderHandler.sendEmptyMessage(UPDATE_FOR_RECEIVED_STATUS_READ);
+        mSenderHandler.sendEmptyMessage(UPDATE_FOR_RECEIVED_STATUS_READ);
     }
 }
